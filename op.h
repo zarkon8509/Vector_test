eac57d19285c7b7e202e55fea347765d1df56bbd
zarkon8509 <zarkon85@gmail.com>
2022-04-20 13:51:21 +0200

    1350

--- op.h
+++ op.h
@@ -2,18 +2,31 @@
 
 #include<iostream>
 #include<vector>
+#include<ctime>
+
+//SFML
+#include<SFML/Graphics.hpp>
+#include<SFML/Window.hpp>
+#include<SFML/System.hpp>
+
+class object;
 
 class op
 {
     private:
-    std::vector<int> a;
+    //std::vector<int> a;
+    
     
 
     public:
 
     op();
+    virtual ~op();
 
-    void setVect(int i);
+    
+    void setVect(std::vector<object>* obj);
     void multiply();
     void print();
+
+    
 };
